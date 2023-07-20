990b0e5e0303810c4cd0fec0c1f435481c7e49b8
Ekaterina Kilina <kilina2018@bk.ru>
2023-07-20 17:09:58 +0600

    refactoring brain-calc.js and index.js

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,61 +1,37 @@
 #!/usr/bin/env nodejs
 
 import readlineSync from 'readline-sync';
+import {userAnswerCheckNumber, getRandomInt} from '../../src/index.js';
 
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
+console.log('What is the result of the expression?');
 
-let levelPass = true;
+const expressions = [];
+const answers = [];
+const operations = ['+', '-', '*'];
 
 for (let i = 0; i < 3; i += 1) {
-  if (levelPass === false) {
-    break;
-  }
-
-  const getRandomInt = (bottom, top) => {
-    const min = Math.ceil(bottom);
-    const max = Math.floor(top);
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  const mathOperations = ['+', '-', '*'];
-  const operation = mathOperations[getRandomInt(0, 2)];
-
-  const num1 = Math.floor(Math.random() * 10);
-  const num2 = Math.floor(Math.random() * 10);
-
-  const expression = `${num1} ${operation} ${num2}`;
-
+  const randomIndex = getRandomInt(0, 2);
+  const randomNum1 = getRandomInt(0, 10);
+  const randomNum2 = getRandomInt(0, 10);
+  expressions.push(`Question: ${randomNum1} ${operations[randomIndex]} ${randomNum2}`);
   let answer = '';
-
-  switch (operation) {
+  switch (operations[randomIndex]) {
     case '+':
-      answer = num1 + num2;
+      answer = randomNum1 + randomNum2;
       break;
     case '-':
-      answer = num1 - num2;
+      answer = randomNum1 - randomNum2;
       break;
     case '*':
-      answer = num1 * num2;
+      answer = randomNum1 * randomNum2;
       break;
     default:
       break;
   }
-
-  console.log(expression);
-  const userAnswer = readlineSync.question('Your answer: ');
-
-  if (Number(userAnswer) === Number(answer)) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
-    console.log(`Let's try again, ${name}!`);
-    levelPass = false;
-  }
+  answers.push(answer);
 }
 
-if (levelPass) {
-  console.log(`Congratulations, ${name}!`);
-}
+userAnswerCheckNumber(expressions, answers, name);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // тут логика всех игр
 import readlineSync from 'readline-sync';
 
-const userAnswerCheck = (expressions, answers, name) => {
+const userAnswerCheckString = (expressions, answers, name) => {
   let levelPass = true;
   for (let i = 0; i < 3; i += 1) {
     if (levelPass === false) {
@@ -22,6 +22,27 @@ const userAnswerCheck = (expressions, answers, name) => {
   }
 }
 
+const userAnswerCheckNumber = (expressions, answers, name) => {
+  let levelPass = true;
+  for (let i = 0; i < 3; i += 1) {
+    if (levelPass === false) {
+      break;
+    }
+    console.log(expressions[i]);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (Number(userAnswer) === Number(answers[i])) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answers[i]}'.`);
+      console.log(`Let's try again, ${name}!`);
+      levelPass = false;
+    }
+  }
+  if (levelPass) {
+    console.log(`Congratulations, ${name}!`);
+  }
+}
+
 const getRandomInt = (bottom, top) => {
   const min = Math.ceil(bottom);
   const max = Math.floor(top);
@@ -29,4 +50,4 @@ const getRandomInt = (bottom, top) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { userAnswerCheck, getRandomInt };
+export { userAnswerCheckString, userAnswerCheckNumber, getRandomInt };
