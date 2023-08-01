0fab0d9ea9c1a77eec8ff2be19f69a894f7b823d
Ekaterina Kilina <kilina2018@bk.ru>
2023-08-01 20:58:52 +0600

    fix task in brain-prime.js

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -7,7 +7,7 @@ import { getRandomInt } from '../../src/utils.js';
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 const expressions = [];
 const answers = [];
